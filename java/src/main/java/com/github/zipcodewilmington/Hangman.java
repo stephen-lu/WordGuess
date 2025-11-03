f828cf4011a40a1c56a79b0019a433ee47a0a114
Stephen <stephen.lu20@gmail.com>
2025-11-03 16:38:03 -0500

    Added displaying wrong guesses. Added checks for repeat guess. Added display for current lives, wrong letters, and current state the guess. Added display for losing

--- java/src/main/java/com/github/zipcodewilmington/Hangman.java
+++ java/src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -20,6 +20,7 @@ public class Hangman {
     public Hangman() {
         this.lives = 6;
         this.guess = new ArrayList<String>();
+        this.hints = new ArrayList<String>();
     }
 
     public void runHangman() {
@@ -32,21 +33,33 @@ public class Hangman {
             scanner = new Scanner(System.in);
             System.out.println("Welcome to Hangman");
             Random random = new Random();
-            this.word = "testing"; //words.get(random.nextInt(words.size()));
+            this.word = words.get(random.nextInt(words.size()));
             int wordLength = this.word.length();
             for (int i = 0; i < wordLength; i++) {
                 this.guess.add("_");
             }
 
             while (true) {
-                for (int i = 0; i < this.guess.size(); i++) {
-                    System.out.print(this.guess.get(i) + " ");
-                }
                 System.out.println();
+                System.out.println("Current Lives: " + Integer.toString(this.lives));
                 if (this.lives == 0) {
+                    System.out.println("The word was " + this.word);
                     System.out.println("You lose");
                     return;
                 }
+                System.out.print("Wrong Letters: ");
+                for (int i = 0; i < this.hints.size(); i++) {
+                    System.out.print(this.hints.get(i) + " ");
+                }
+                System.out.println();
+                for (int i = 0; i < this.guess.size(); i++) {
+                    System.out.print(this.guess.get(i) + " ");
+                }
+                if (String.join("", this.guess).equals(this.word)) {
+                    System.out.println("\nYou win");
+                    return;
+                }
+                System.out.println();
                 System.out.print("> ");
                 String userInput = scanner.nextLine().trim();
                 if (userInput.isEmpty()) { System.out.println(); continue; }
@@ -67,8 +80,17 @@ public class Hangman {
                 } else if (userInput.length() == 0) {
                     continue;
                 } else {
-                    System.out.println("Incorrect");
-                    this.lives--;
+                    if (userInput.length() == 1) {
+                        if (Character.isLetter(userInput.charAt(0))) {
+                            if (!this.hints.contains(userInput)) {
+                                this.hints.add(userInput);
+                                System.out.println("Incorrect");
+                                this.lives--;
+                            }
+                        } else {
+                            continue;
+                        }
+                    }
                 }
             }
         } finally {
